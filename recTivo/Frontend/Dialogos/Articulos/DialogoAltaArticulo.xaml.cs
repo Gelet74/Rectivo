8704ddcda5b596e98e5c93723f2830f91861d889
Gelet74 <sanchis74@gmail.com>
2025-12-16 09:40:20 +0100

    16/12 09:40

--- recTivo/Frontend/Dialogos/Articulos/DialogoAltaArticulo.xaml.cs
+++ recTivo/Frontend/Dialogos/Articulos/DialogoAltaArticulo.xaml.cs
@@ -1,5 +1,7 @@
 ﻿
+using recTivo.Frontend.Dialogos.VentanasInicio;
 using System.Windows;
+using System.Windows.Input;
 
 
 namespace recTivo.Frontend.Dialogos.Articulos
@@ -21,6 +23,41 @@ namespace recTivo.Frontend.Dialogos.Articulos
         private void btnAltaArticulo_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private bool _escapeEnCurso = false;
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                if (_escapeEnCurso)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                _escapeEnCurso = true;
+                e.Handled = true;
+
+                try
+                {
+                    var dialog = new ConfirmacionDialogo { Owner = this };
+                    bool? result = dialog.ShowDialog();
+
+                    if (result == true && dialog.Confirmado)
+                    {
+                        this.Close();
+                    }
+                }
+                finally
+                {
+                    _escapeEnCurso = false;
+                }
+            }
+            else
+            {
+                base.OnPreviewKeyDown(e);
+            }
         }
     }
 }
